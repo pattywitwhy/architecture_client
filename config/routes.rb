222359ec2003a117cf3af39a14d0df2e31f43f4d
pattywitwhy <pattyxhwang@gmail.com>
2019-02-11 08:42:19 -0600

    building model created

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   namespace :client do
     get '/buildings' => 'buildings#index'
     get '/buildings/new' => 'buildings#new'
-    get '/buildings/:id' => 'buildings#show'
     post '/buildings' => 'buildings#create'
+    get '/buildings/:id' => 'buildings#show'
     get '/buildings/:id/edit' => 'buildings#edit'
     patch '/buildings/:id' => 'buildings#update'
     delete '/buildings/:id' => 'buildings#destroy'
